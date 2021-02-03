163f645d9624539a49accc56be96173486b9ab94
SD <sd@SDs-MacBook-Pro.local>
2021-02-03 03:04:02 +0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
-  # Returns the current logged-in user (if any).
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
@@ -27,6 +27,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+  
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
   
   # Returns true if the user is logged in, false otherwise.
@@ -46,5 +51,10 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+  
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
